5801423ccfd726fab96a81355d120f08996a9a40
pntien <42037314+pntien@users.noreply.github.com>
2020-06-26 19:17:42 +0700

    Add Extra Exception

---
+++ KSERPUtilities/Exceptions/KSERPExceptions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KSERPUtilities.Exceptions
+{
+    public class KSERPExceptions : Exception
+    {
+        public KSERPExceptions()
+        {
+
+        }
+        public KSERPExceptions(string message) : base(message)
+        {
+
+        }
+        public KSERPExceptions(string message, Exception inner) : base(message, inner) { }
+    }
+}
